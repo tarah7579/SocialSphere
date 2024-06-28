03f827c4d8b357ebe64a340241d4f04a18dd9483
Trisan Españo <121277075+Wrecage@users.noreply.github.com>
2024-06-28 22:15:25 +0800

    Update facebook_config.py

--- SocialSphere/facebook_config.py
+++ SocialSphere/facebook_config.py
@@ -2,34 +2,34 @@
 # Facebook Graph API
 # import facebook as fb
 #Short Lived
-# access_token="EAAN1kk9bS5cBOxDzlpNxnRcJGu0P1oM1L7SsAtck9Mzl1ojLF6ZAC0aDJ4egm63gjawrxBIm2s3wGkGoCbEPx0moINqlrHql0qDZAZBvmKXQEAqeSsnZApRoAFoOZAppkmT8AsILyMmeqAfWZBALPLq9eAOnwtUZAJfAD497mAKpkyQ3XLgpOOuwCFHouHZBmaYJS0aU4uyhZAbtJh2ZB4EWnHCVcZD"
+# access_token=""
 
 
 #LONG LIVED TOKEN
-# access_token="EAAN1kk9bS5cBO4y9WlAea9G46karJ7KlUzad3NN7Is31xJys9Pftf7mOvRY1r3FSvn9IHuR7GSyIkKID12RRJfdFxNmXRN3p8NCFgvZCPiwEfALCTT0q4eN054O7N6rNZBClqHfAsnUxX3Pm0HAlaSUjH5yZBSnRQzLZBd2DrdHv33GBgStNCI6X"
+# access_token=""
 
 
 
 # Page ID
 # {
-#   "id": "254720957735057",
+#   "id": "",
 #   "name": "SocialSphere"
 # }
 
-#Page id:254720957735057
+#Page id:
 
-#App ID: 973696187190167 
-#App secret key: 31dd98baff1ca39c8bf40e98dc030a16
+#App ID:  
+#App secret key: 
 
 #LONG LIVED TOKEN PARAMS
 # {
-#   "access_token": "EAAN1kk9bS5cBO9kJfHejgMg4XLNZActtz8OKZAO3x3EZAujlctkz7Cc05RBEc7mPmou26EJtZBRw3yWxruoYeVf95ISSn1EzhtW7TZBCZBXmVbp9yU3gyoEtH97qXi3sEEnNPpbUIxqiROln8f5v0DMIYqO97eAFy5t52ENZA90u3342G0vaQGl9eZAZBo4qnvoYZD",
-#   "token_type": "bearer"
+#   "access_token": "",
+#   "token_type": ""
 # }
 
 
 
-app_id = '973696187190167'
-app_secret = '31dd98baff1ca39c8bf40e98dc030a16'
-ACCESS_TOKEN = 'EAAN1kk9bS5cBO9kJfHejgMg4XLNZActtz8OKZAO3x3EZAujlctkz7Cc05RBEc7mPmou26EJtZBRw3yWxruoYeVf95ISSn1EzhtW7TZBCZBXmVbp9yU3gyoEtH97qXi3sEEnNPpbUIxqiROln8f5v0DMIYqO97eAFy5t52ENZA90u3342G0vaQGl9eZAZBo4qnvoYZD'
-PAGE_ID = '254720957735057'
+app_id = ''
+app_secret = ''
+ACCESS_TOKEN = ''
+PAGE_ID = ''
