6a2491e65af2b51f71500c8954aac5ee88e2530f
wrecage <121277075+Wrecage@users.noreply.github.com>
2024-10-23 18:19:33 +0800

    HAHAYZZ NAUNSI NAMAN NI

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -168,9 +168,16 @@ USE_TZ = True
 
 # Static file settings
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
