e0e803683adabdafde74cd4e7e8bf728193502cb
wrecage <121277075+Wrecage@users.noreply.github.com>
2024-10-23 15:25:14 +0800

    remove pywin32
    remove pywin32

--- Capstone/wsgi.py
+++ Capstone/wsgi.py
@@ -14,3 +14,38 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Capstone.settings')
 
 application = get_wsgi_application()
+
+
+# PYTHON ANYWHERE CONFIGURATION
+
+
+
+# import os
+# import sys
+
+
+# path = '/home/SocialSphere/SocialSphere'
+# if path not in sys.path:
+#     sys.path.append(path)
+
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'Capstone.settings'
+
+# # then:
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
+
+
+
+# What your site is running.
+
+# Source code:
+# /home/SocialSphere/SocialSphere
+
+# Go to directory
+# Working directory:
+# /home/SocialSphere/
+
+# Go to directory
+# WSGI configuration file:/var/www/socialsphere_pythonanywhere_com_wsgi.py
+# Python version:
+# 3.10
